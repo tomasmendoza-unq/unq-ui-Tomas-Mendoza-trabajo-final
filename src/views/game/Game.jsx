4bc376e631fd3ba4eb86babdb14d8dd6ba7f24e3
Tomas Mendoza <tm1453766@gmail.com>
2026-07-13 13:05:05 -0300

    feature: se chequea si la palabra ingresada esta dentro de las ingresadas, si no se guarda

--- src/views/game/Game.jsx
+++ src/views/game/Game.jsx
@@ -5,6 +5,23 @@ import "./style/Game.css";
 export const Game = () => {
     const [time, setTime] = useState(15);
     const [words, setWords] = useState([]);
+    const [currentWord, setCurrentWord] = useState("");
+
+    const onChangeInput = (e) => {
+        const word = e.target.value.trim();
+        setCurrentWord(word);
+        console.log("Palabra ingresada:", word);
+    };
+
+    const onSubmitWord = (e) => {
+        e.preventDefault();
+        if (words.includes(currentWord)) {
+            console.log("Palabra ya ingresada:", currentWord);
+        } else {
+            setWords([...words, currentWord]);
+            setCurrentWord("");
+        }
+    };
 
     return (
         <section className="game">
@@ -12,11 +29,25 @@ export const Game = () => {
                 time={time}
                 setTime={setTime}
             />
-            <input
-                type="text"
-                className="word-input"
-                placeholder="Ingresa una palabra"
-            />
+            <form
+                className="word-form"
+                onSubmit={onSubmitWord}
+            >
+                <input
+                    type="text"
+                    id="word"
+                    className="word-input"
+                    placeholder="Ingresa una palabra"
+                    value={currentWord}
+                    onChange={onChangeInput}
+                />
+                <button
+                    type="submit"
+                    className="submit-word-button"
+                >
+                    Enviar
+                </button>
+            </form>
         </section>
     );
 };
