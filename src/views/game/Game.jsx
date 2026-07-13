ed266ed434550e5d9a0387f692950a80932d0cfe
Tomas Mendoza <tm1453766@gmail.com>
2026-07-13 12:40:12 -0300

    feature: genero un timer usando interval

--- src/views/game/Game.jsx
+++ src/views/game/Game.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { Timer } from "./components/Timer";
 import "./style/Game.css";
 
 export const Game = () => {
+    const [time, setTime] = useState(15);
+    const [words, setWords] = useState([]);
+
     return (
         <section className="game">
-            <Timer />
+            <Timer
+                time={time}
+                setTime={setTime}
+            />
             <input
                 type="text"
                 className="word-input"
